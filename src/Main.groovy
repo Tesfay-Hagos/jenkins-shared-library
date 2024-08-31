73a93c735d2aa9fcc991a616da24e378f64d2bd8
Tesfay Hagos <tesfay.hagos1421@gmail.com>
2024-08-31 23:48:19 +0300

    this is test parameter definition

--- src/Main.groovy
+++ src/Main.groovy
@@ -1,4 +1,7 @@
 #!usr/bin/env groovy
+parameters{
+  string(name:'test',defaultValue:'Hello test',description:'this is test parameter')
+}
 static void main(String[] args) {
   println "Hello world!"
 }
--- vars/BuildApp.groovy
+++ vars/BuildApp.groovy
@@ -4,6 +4,6 @@ def call(){
         error('Version parameter is required')
     } else {
         echo 'Building docker image...'
-        sh "docker build -t tesfayh2121/manage-container:${params.Version} -f ./Dockerfile ."
+        sh "docker build -t ${params.ImageName}:${params.Version} -f ./Dockerfile ."
     }
 }
