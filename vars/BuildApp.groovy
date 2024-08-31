e1c9bcbdf8cda24356cb9bcde7a992d7a7638694
Tesfay Hagos <tesfay.hagos1421@gmail.com>
2024-09-01 00:20:13 +0300

    add arguments to the groovy shared functions to make them generic

--- vars/BuildApp.groovy
+++ vars/BuildApp.groovy
@@ -1,9 +1,10 @@
 #!usr/bin/env groovy
-def call(){
+//def call(String imageName,String version,String dockerfilePath){
+def call(String imageName, String version,String dockerfilePath) {
     if (params.Version == '') {
         error('Version parameter is required')
     } else {
         echo 'Building docker image...'
-        sh "docker build -t ${params.ImageName}:${params.Version} -f ./Dockerfile ."
+        sh "docker build -t ${imageName}:${version} -f ./${dockerfilePath} ."
     }
 }
