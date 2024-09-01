1f0c25f293d2ec29ba648618149162de37e64dae
Tesfay Hagos <tesfay.hagos1421@gmail.com>
2024-09-01 13:44:13 +0300

    add server to docker push

--- vars/DockerPush.groovy
+++ vars/DockerPush.groovy
@@ -2,6 +2,6 @@
 import com.Docker
 
 //def call(String imageName,String version,String dockerfilePath){
-def call(String imageName, String version) {
-    return new Docker(this).dockerPush(imageName, version)
+def call(String imageName, String version, String server) {
+    return new Docker(this).dockerPush(imageName, version, server)
 }
