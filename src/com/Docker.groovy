d4a93ef35751430aa95009eaac66f6abeb7bb97b
Tesfay Hagos <tesfay.hagos1421@gmail.com>
2024-09-01 13:15:26 +0300

    chore: Refactor Docker.groovy for improved readability and maintainability

--- src/com/Docker.groovy
+++ src/com/Docker.groovy
@@ -9,27 +9,25 @@ class Docker {
     }
 
     def buildDockerImage(String imageName, String version, String dockerfilePath) {
-        echo "Building Docker image ${imageName}: ${version}..."
+        script.echo "Building Docker image ${imageName}: ${version}..."
         if (version == '') {
-            error('Version parameter is required')
-    } else {
-            echo 'Building docker image...'
-            sh "docker build -t ${imageName}:${version} -f ./${dockerfilePath} ."
+            script.error('Version parameter is required')
+        } else {
+            script.echo 'Building docker image...'
+            script.sh "docker build -t ${imageName}:${version} -f ./${dockerfilePath} ."
         }
     }
-    def dockerLogin(String credentials, String server) {
-        echo 'Docker login...'
 
-        // To separate get the password and the username use functions as below
-        withCredentials([usernamePassword(credentialsId: credentials ,
-     usernameVariable: 'USER', passwordVariable: 'PWD')]) {
-            // sh "some script ${USER} ${PWD}"  // No script for now
-            sh "echo ${PWD} | docker login -u ${USER} --password-stdin ${server}"
-     }
+    def dockerLogin(String credentials, String server) {
+        script.echo 'Docker login...'
+        script.withCredentials([script.usernamePassword(credentialsId: credentials, usernameVariable: 'USER', passwordVariable: 'PWD')]) {
+            script.sh "echo ${PWD} | docker login -u ${USER} --password-stdin ${server}"
+        }
     }
+
     def dockerPush(String imageName, String version) {
-        echo "Pushing to ${server}"
-        sh "docker push ${imageName }:${ version }"
+        script.echo "Pushing to ${server}"
+        script.sh "docker push ${imageName}:${version}"
     }
 
 }
